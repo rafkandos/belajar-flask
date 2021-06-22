72c85fff38e3dcd8d018103f0edfa5ce204085a6
Raffli <raffli@algostudio.net>
2021-06-22 21:22:00 +0700

    materi Flask 2

--- myapp.py
+++ myapp.py
@@ -1,10 +1,14 @@
-from flask import Flask;
+from flask import Flask, render_template;
 
 app = Flask(__name__)
 
 @app.route("/index")
 def home():
-	return "Halo ini raffli"
+	# Looping
+	hari = ['senin', 'selasa', 'rabu', 'kamis', 'jumat', 'sabtu', 'minggu']
+	# Conditional
+	ujian = "lulus" # Jika lulus saya akan senang, jika tidak lulus saya sedih
+	return render_template("index.html", hari=hari, ujian=ujian)
 
 	if __name__ == "__main__":
 		app.run(debug=True)
